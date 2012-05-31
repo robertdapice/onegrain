5622dd7a26d89ef7a7b8deac53a8d5e44d4d0d1a
Robert d'Apice <robertdapice@gmail.com>
2012-05-31 14:11:53 +1000

    initial commit for onegrain

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onegrain::Application.config.secret_token = 'fee0142d30f8f10b6fd0b29e7cb3efd36ded6522a479873504dafa11e8555309f548d45833fea31c076ad3027cf5f1ad09a7403574bc3da7c8bc8c2824fe22b9'
