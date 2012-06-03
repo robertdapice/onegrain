5338e47964bcd7d37c5fd097b3bd5a120230e9b8
Robert d'Apice <robertdapice@gmail.com>
2012-06-03 10:20:42 +1000

    fixed query parameters for data.json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,12 @@ class PagesController < ApplicationController
   end
 
   def data
+    if params[:name]
+      @budget_items = BudgetItem.where(:parent_id => BudgetItem.find_by_name(params[:name]).id)
+    else
+      @budget_items = BudgetItem.where(:parent_id => nil)
+    end
+    @year = params[:year] || "12_13"
     render 'data'
   end
 end
--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -2,13 +2,13 @@ class BudgetItem < ActiveRecord::Base
   belongs_to :parent, :class_name => "BudgetItem"
   has_many :children, :class_name => "BudgetItem", :foreign_key => "parent_id"
 
-  def with_children
+  def with_children(year = "12_13")
     data = {}
     data[:name] = self.name
     if self.children.length > 0
-      data[:children] = self.children.map{|child| child.with_children}
+      data[:children] = self.children.map{|child| child.with_children(year)}
     else
-      data[:size] = self.value12_13
+      data[:size] = self.send("value" + year)
     end
     data
   end
