b34eafcea9b8a7b704c1de0ecf30b65501147bfb
Robert d'Apice <robertdapice@gmail.com>
2012-06-05 12:38:25 +1000

    tried to fix reference to d3 on production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require d3.v2.min
 
 
 function populateSidebar(budgetItem) {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Onegrain::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -50,6 +50,9 @@ Onegrain::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
+
+  config.assets.precompile += %w[d3.v2.min.js]
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
